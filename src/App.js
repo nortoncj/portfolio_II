cad24faffcef13d4f272e491fc80c4e86c3f2a99
Chris Norton <officialantiviral@gmail.com>
2022-01-19 22:45:50 -0500

    nav and about

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from "./logo.svg";
+import avi from "./img/Watermark.png";
 import "./styles.css";
+import "../node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <div className="hero px-10 py-5">
+        <nav className="d-flex flex-row justify-content-between">
+          <span className="brand mx-5">KRVTZ</span>
+          <div>
+            <span className="mx-4 nav-menu">Home</span>
+            <span className="mx-4 nav-menu">About Me</span>
+            <span className="mx-4 nav-menu">Resume</span>
+            <span className="mx-4 nav-menu">Testimonial</span>
+            <span className="mx-4 nav-menu">Contact</span>
+          </div>
+        </nav>
+        <div className="hero-2 row justify content-center">
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <div className="d-flex flex-column justify-content-center intro-section">
+              <span className="I-am">
+                Hi, my name is <span className="name">Chris Norton</span>
+              </span>
+              <span className="intro-adj py-2">Industrious Dev</span>
+              <span className="intro-subheading">
+                Specialized in front end and back end applications
+              </span>
+              <div className="d-flex flex-row justify-content-center mt-5">
+                <button className="btn-hire">Hire Me</button>
+                <button className="btn-cv">
+                  <i class="fal fa-arrow-down" id="down"></i> CV
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12">
+            <div className="d-flex justify-content- center avi">
+              <img className="img-fluid" alt="heroimage" src={avi}></img>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+      </div>
+
+      <div className="about-me-section aboutme my-5">
+        <div className="d-flex flex-column">
+          <span className="about-me-text"> About Me </span>
+          <div className="row justify-content center shadow-lg my-5 about-me-pannel">
+            <div className="col-lg-6 col-md-6 col-sm-12 d-flex align-items-center justify-content-center d-flex flex-column">
+              <lottie-player
+                src="https://assets8.lottiefiles.com/packages/lf20_fs006iff.json"
+                background="transparent"
+                speed="1"
+                style={{ width: "500px", height: "500px" }}
+                loop
+                autoplay
+              ></lottie-player>
+              <span className="quote">
+                "Desire! That's the one secret of every man's career. Not
+                education. Not being born with hidden talents. Desire" - Johnny
+                Carson
+              </span>
+            </div>
+            <div className="col-lg-6 col-md-6 col-sm-12 py-5 d-flex flex-column">
+              <span className="about-txt">
+                My developer journey began while I was studying Engineering in
+                college. While learning basic programming concepts in
+                university, I took an interest in learning to build complex
+                stand-alone applications on my own. I eventually grew to work on
+                multiple web applications for various clients.
+                <br />I have developed a deep love for learing about development
+                in both hardware and software. A more detailed look at my
+                experices can be seen in my <span>resume</span>
+              </span>
+              <span className="highlights">Skills</span>
+              <span className="skills">
+                <ul>
+                  <li>JavaScript (ES6+), Node, React</li>
+                  <li>Java</li>
+                  <li>C#, ASP</li>
+                  <li>PHP</li>
+                  <li>Python, DJango</li>
+                  <li>MongoDB, MYSQL</li>
+                </ul>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="resume-section d-flex flex-column">
+        <span className="about-me-text">Resume</span>
+        <div className="col-lg-4 col-md-4"></div>
+        <div className="col-lg-8 col-md-8"></div>
+      </div>
+    </html>
   );
 }
 
